e2f432d76ae529d34b551e12dd8ab6a88fcb4fdf
leonardo <leonardolfdev@gmail.com>
2025-11-16 19:03:15 -0300

    feat(p3/e4): tecnicamente resolvido

--- p3/e4/code.c
+++ p3/e4/code.c
@@ -3,46 +3,39 @@
 
 typedef struct Node
 {
-    int num;
+    int *num;
+    int length;
     struct Node *next;
 } Node;
 
-void insert(Node **head, int num, char order)
+int cmpAsc(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
+int cmpDesc(const void *a, const void *b)
+{
+    return (*(int *)b - *(int *)a);
+}
+
+void insert(Node **head, int *num, int length, char order)
 {
     Node *newNode = malloc(sizeof(Node));
     newNode->num = num;
     newNode->next = NULL;
+    newNode->length = length;
+
+    qsort(num, length, sizeof(int), order == 'a' ? cmpAsc : cmpDesc);
 
     if (*head == NULL)
     {
         *head = newNode;
-        return;
     }
-
-    for (Node *prev = NULL, *tmp = *head; 1; prev = tmp, tmp = tmp->next)
+    else
     {
-        if (tmp == NULL)
-        {
-            prev->next = newNode;
-            break;
-        }
-
-        if (order == 'a' ? num > tmp->num : num < tmp->num)
-            continue;
-
-        newNode->next = tmp;
-
-        if (prev != NULL)
-        {
-            prev->next = newNode;
-        }
-        else
-        {
-            *head = newNode;
-        }
-
-        break;
-    };
+        newNode->next = (*head)->next;
+        (*head)->next = newNode;
+    }
 }
 
 int main()
@@ -54,6 +47,14 @@ int main()
     Node *headNodeImpar = NULL;
     Node *headNodePar = NULL;
 
+    int sizeBatch = caseLength;
+
+    int *arrayPar = malloc(sizeof(int) * sizeBatch);
+    int countPar = 0;
+
+    int *arrayImpar = malloc(sizeof(int) * sizeBatch);
+    int countImpar = 0;
+
     for (int caseI = 1; caseI <= caseLength; caseI++)
     {
         int result;
@@ -61,22 +62,52 @@ int main()
 
         if (result % 2)
         {
-            insert(&headNodeImpar, result, 'd');
+            arrayImpar[countImpar++] = result;
+
+            if (countImpar >= sizeBatch)
+            {
+                insert(&headNodeImpar, arrayImpar, countImpar, 'd');
+                arrayImpar = malloc(sizeof(int) * sizeBatch);
+                countImpar = 0;
+            }
         }
         else
         {
-            insert(&headNodePar, result, 'a');
+            arrayPar[countPar++] = result;
+
+            if (countPar >= sizeBatch)
+            {
+                insert(&headNodePar, arrayPar, countPar, 'a');
+                arrayPar = malloc(sizeof(int) * sizeBatch);
+                countPar = 0;
+            }
         }
+    }
+
+    if (countImpar)
+    {
+        insert(&headNodeImpar, arrayImpar, countImpar, 'd');
+    }
+    if (countPar)
+    {
+        insert(&headNodePar, arrayPar, countPar, 'a');
     }
 
     for (Node *tmp = headNodePar; tmp != NULL; tmp = tmp->next)
     {
-        printf("%d\n", tmp->num);
+        for (size_t i = 0; i < tmp->length; i++)
+        {
+            printf("%d\n", tmp->num[i]);
+        }
     }
 
     for (Node *tmp = headNodeImpar; tmp != NULL; tmp = tmp->next)
     {
-        printf("%d\n", tmp->num);
+
+        for (size_t i = 0; i < tmp->length; i++)
+        {
+            printf("%d\n", tmp->num[i]);
+        }
     }
 
     return 0;
